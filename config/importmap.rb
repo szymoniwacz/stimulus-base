3a294a7f86be48b404ddd538676cb9ea67a3193e
Szymon Iwacz <szymon@iwacz.pl>
2024-06-27 14:56:10 +0200

    Update importmap.rb

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,5 @@
-# Pin npm packages by running ./bin/importmap
+# frozen_string_literal: true
 
-pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
+pin 'application', preload: true
+pin 'bootstrap', to: 'https://ga.jspm.io/npm:bootstrap@5.0.0/dist/js/bootstrap.esm.js'
+pin '@popperjs/core', to: 'https://unpkg.com/@popperjs/core@2.9.2/dist/esm/index.js'
