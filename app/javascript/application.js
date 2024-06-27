39c8e5449e0e12fc952b976e03513af10a05a507
Szymon Iwacz <szymon@iwacz.pl>
2024-06-27 17:49:00 +0200

    Stimulus fix

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,17 @@
 import '@hotwired/turbo-rails'
 import 'controllers'
 import 'bootstrap'
+import { Turbo } from "@hotwired/turbo-rails"
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
 import Rails from "@rails/ujs"
+import "bootstrap"
+
 Rails.start()
 
+const application = Application.start()
+const context = require.context("controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
+
 console.log("Rails UJS has started")
+
