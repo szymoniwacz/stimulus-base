2ba0f3b9a46eff2a4836e21d09a8de1895412818
Szymon Iwacz <szymon@iwacz.pl>
2024-06-27 17:37:31 +0200

    Transform views into Stimulus

---
+++ app/javascript/controllers/navigation_controller.js
@@ -0,0 +1,9 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  connect() {
+    console.log("Navigation controller connected")
+  }
+
+  // Add methods to handle navigation or other dynamic behaviors here
+}
